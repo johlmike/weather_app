bb5fcccbc3e74a9aff9d37531727508e86f3b395
Johlmike <johlmike@gmail.com>
2016-07-30 22:17:36 +0800

    make server work on localhost

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
+    if (req.headers['x-forwarded-proto'] === 'http' || req.hostname === 'localhost') {
         next();
     } else {
-        res.redirect('http://' + req.hostname + req.url);
+        res.redirect('http://' + req.hostname + ':' + PORT + req.url);
     }
 });
 
