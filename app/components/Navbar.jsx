b713a5a207a4ab8391c53f043fea464f0d5de38e
Johlmike <johlmike@gmail.com>
2016-07-31 05:41:50 +0800

    Improve the Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,15 +1,50 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Navbar = (props) => {
-    return (
-        <div>
-            <h2>React Weather</h2>
-            <IndexLink to='/' activeClassName='active' activeStyle={{fontSize: '30px'}}>Weather</IndexLink>
-            <Link to='about' activeClassName='active' activeStyle={{fontSize: '30px'}}>About</Link>
-            <Link to='example' activeClassName='active' activeStyle={{fontSize: '30px'}}>Example</Link>
-        </div>
-    );
-};
+var Navbar = React.createClass({
+    onSearch: function(e) {
+        e.preventDefault();
+        console.log('Not Yet');
+    },
+    render: function() {
+        return (
+            <div className='top-bar'>
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather</li>
+                        <li>
+                            <IndexLink to='/' activeClassName='active' activeStyle={{
+                                fontWeight: 'bold'
+                            }}>Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to='about' activeClassName='active' activeStyle={{
+                                fontWeight: 'bold'
+                            }}>About</Link>
+                        </li>
+                        <li>
+                            <Link to='example' activeClassName='active' activeStyle={{
+                                fontWeight: 'bold'
+                            }}>Example</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search Weather"/>
+                            </li>
+                            <li>
+                                <button type="submit" className="button">Get Weather</button>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+
+            </div>
+        );
+    }
+});
 
 module.exports = Navbar;
